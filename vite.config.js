d61844fe4f525f7258ffaeb0f0fb56ec3978cc1f
Erik-Mfa <erikmfa190230@gmail.com>
2025-09-13 02:33:14 -0400

    version 1.1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Township-Green/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Township-Green-3/' : '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
